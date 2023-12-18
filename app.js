7a6fbcfff31a45a2a979155374202593f7645cde
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-12-18 17:01:39 +0100

    dev commit

--- app.js
+++ app.js
@@ -1,128 +1,27 @@
-/*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+const express = require('express');
+const app = express();
+const router = express.Router();
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+const path = __dirname + '/public/';
+const port = 8080;
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-app.get('/', (req, res) => {
-	res.send('CDI Scheduler App');
+router.use(function (req, res, next) {
+  console.log('/' + req.method);
+  next();
 });
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-console.log('appEnv');
-console.log(appEnv);
-
-console.log('process.env');
-console.log(process.env);
-
-// define related application URLs
-var kpes_url = process.env.kpes_url;
-var rest_url = process.env.rest_url;
-var vbac_url = process.env.vbac_url;
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function () {
-	// print a message when the server starts listening
-	console.log("server starting on " + appEnv.url);
+router.get('/', function (req, res) {
+  res.sendFile(path + 'index.html');
 });
 
-var schedule = require('node-schedule');
+router.get('/subPage', function (req, res) {
+  res.sendFile(path + 'subpage.html');
+});
 
-var testRule = new schedule.RecurrenceRule();
-testRule.hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-testRule.minute = [0, 10, 20, 30, 40, 50];
+app.use(express.static(path));
+app.use('/', router);
 
-console.log(testRule);
-
-/*
-try {
-	var km = schedule.scheduleJob(testRule, function(){
-		console.log('About to run kPES testEmail');
-		console.log(kpes_url);
-		var request = require('request');
-		request(kpes_url + 'batchJobs/testEmail.php', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				console.log('kPES - Test Email Sent');
-				console.log(body); // Print the google web page.
-			} else {
-				console.log('Error sending Test Email - kPES');
-				console.log(error);
-				console.log(response);
-				console.log(body);
-			}
-		});
-	});
-} catch (error) {
-	console.error(error);
-	// Expected output: ReferenceError: nonExistentFunction is not defined
-	// (Note: the exact output may be browser-dependent)
-}
-*/
-
-/*
-try {
-	var vm = schedule.scheduleJob(testRule, function () {
-		console.log('About to run vBAC testEmail');
-		console.log(vbac_url);
-		var request = require('request');
-		request(vbac_url + 'batchJobs/testEmail.php', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				console.log('vBAC - Test Email Sent');
-				console.log(body); // Print the google web page.
-			} else {
-				console.log('Error sending Test Email - vBAC');
-				console.log(error);
-				console.log(response);
-				console.log(body);
-			}
-		});
-	});
-} catch (error) {
-	console.error(error);
-	// Expected output: ReferenceError: nonExistentFunction is not defined
-	// (Note: the exact output may be browser-dependent)
-}
-*/
-
-/*
-try {
-	var rm = schedule.scheduleJob(testRule, function () {
-		console.log('About to run REST testEmail');
-		console.log(rest_url);
-		var request = require('request');
-		request(rest_url + 'batchJobs/testEmail.php', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				console.log('REST - Test Email Sent');
-				console.log(body); // Print the google web page.
-			} else {
-				console.log('Error sending Test Email - REST');
-				console.log(error);
-				console.log(response);
-				console.log(body);
-			}
-		});
-	});
-} catch (error) {
-	console.error(error);
-	// Expected output: ReferenceError: nonExistentFunction is not defined
-	// (Note: the exact output may be browser-dependent)
-}
-*/
-
-console.log('Creating the scheduleJob entries - ARO Dev');
-
-console.log('scheduleJob entries created');
+app.listen(port, function () {
+  console.log('UK Business Intelligence sample app 1 listening on port 8080!')
+})
--- docker_build_base.bat
+++
@@ -1,2 +0,0 @@
-docker build . -t cdi-scheduler
-docker run -p 8087:8080 -d cdi-scheduler
